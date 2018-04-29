7df94ded46dfc7ce15b23630e312d91e3f5895fc
Peter John <peterfirst@gmail.com>
2018-04-29 19:06:59 +0400

    add logger as default

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,7 +1,16 @@
 const _ = require('lodash');
 const debug = require('debug');
 
+const loggerDefault = function (name) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift(`${name}:`);
+    console.log.apply(console, args);
+  };
+};
+
 module.exports = function getLogger(name, logger) {
+  logger = logger || loggerDefault(name);
   const log = debug(name);
   return function () {
     log(...arguments);
